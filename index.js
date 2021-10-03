69af17b2bee99815a96261aba06bbe837e321ef0
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-10-02 23:24:21 -0400

    stuff

---
+++ index.js
@@ -0,0 +1,19 @@
+const btcdata = document.querySelectorAll("[data-Bitcoin]");
+const ethdata = document.querySelectorAll("[data-Ethereum]");
+const carddata = document.querySelectorAll("[data-Cardano]");
+const poldata = document.querySelectorAll("[data-Polkadot]");
+const mondata = document.querySelectorAll("[data-Monero]");
+const steldata = document.querySelectorAll("[data-Stellar]");
+const chaindata = document.querySelectorAll("[data-Chainlink]");
+const tethdata = document.querySelectorAll("[data-Tether]");
+
+const crypto = [
+  { name: "Bitcoin", price: "47508.30", id: 0 },
+  { name: "Etherium", price: "3385.38", id: 1 },
+  { name: "Cardano", price: "2.24", id: 2 },
+  { name: "Polkadot", price: "31.84", id: 3 },
+  { name: "Monero", price: "254.91", id: 4 },
+  { name: "Stellar", price: "0.316", id: 5 },
+  { name: "Chainlink", price: "26.88", id: 6 },
+  { name: "Tether", price: "1.00", id: 7 },
+];
